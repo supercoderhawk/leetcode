a89b1a864d3777aef424a9069bdf57e144b30d80
supercoderhawk <supercoderhawk@gmail.com>
2017-07-24 17:22:02 +0800

    init java language

---
+++ Java/src/main/java/com/supercoderhawk/Solution_1_10.java
@@ -0,0 +1,14 @@
+package com.supercoderhawk;
+
+public class Solution_1_10 {
+  /**
+   * problem 1
+   * @param nums: 输入数组
+   * @param target: 目标和
+   * @return: 目标和的索引数组
+   */
+  public int[] twoSum(int[] nums, int target) {
+    return new int[]{1,2};
+  }
+
+}
