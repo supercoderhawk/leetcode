b1f2fb0c110d2d3b799ae7550a2010a8d8ee0d42
supercoderhawk <supercoderhawk@gmail.com>
2017-07-26 09:17:21 +0800

    add some common definition

--- C/main.h
+++ C/main.h
@@ -1,8 +1,20 @@
 //
-// Created by xyb-C308 on 2017/7/19.
+// Created by supercoderhawk on 2017/7/19.
 //
 
 #ifndef C_MAIN_H
 #define C_MAIN_H
+#define bool char
+#define true 1
+#define false 0
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+struct ListNode *createList(const int *vals, int count);
+
+void printList(struct ListNode *l);
 
 #endif //C_MAIN_H
