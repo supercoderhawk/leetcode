aefdb0742439c03d3fa068b21dbe3739b2a1dd5f
xiayubin <xiayubin@patsnap.com>
2019-08-28 21:39:00 +0800

    add TreeNode

--- Python/common_utils.py
+++ Python/common_utils.py
@@ -32,3 +32,10 @@ class Interval:
     def __init__(self, s=0, e=0):
         self.start = s
         self.end = e
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
