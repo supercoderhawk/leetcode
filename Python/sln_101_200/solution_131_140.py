7e828cf4327a8420ee3422b8e1f8d05c3c2a95aa
supercoderhawk <supercoderhawk@gmail.com>
2019-09-04 23:09:23 +0800

    python: add problem 131 and unittest

--- Python/sln_101_200/solution_131_140.py
+++ Python/sln_101_200/solution_131_140.py
@@ -3,6 +3,27 @@ from typing import List
 
 
 class Solution_131_140:
+    def partition(self, s: str) -> List[List[str]]:
+        """
+        131
+        :param s:
+        :return:
+        """
+        results = []
+
+        def partition_substring(arr, rest_s):
+            if not rest_s:
+                results.append(arr)
+            for i in range(1, len(rest_s) + 1):
+                sub_s = rest_s[:i]
+
+                part_len = i // 2
+                if not part_len or sub_s[:part_len] == sub_s[-part_len:][::-1]:
+                    partition_substring(arr + [sub_s], rest_s[i:])
+
+        partition_substring([], s)
+        return results
+
     def singleNumber(self, nums):
         """
         136
--- Python/sln_101_200/test_solution_131_140.py
+++ Python/sln_101_200/test_solution_131_140.py
@@ -7,6 +7,11 @@ class TestSolution_131_140(TestCase):
     def setUp(self):
         self.sln = Solution_131_140()
 
+    def test_partition(self):
+        self.assertEqual([['a', 'a', 'b'], ['aa', 'b']], self.sln.partition('aab'))
+        self.assertEqual([["a", "b", "b", "a", "b"], ["a", "b", "bab"], ["a", "bb", "a", "b"], ["abba", "b"]],
+                         self.sln.partition('abbab'))
+
     def test_singleNumber(self):
         ret = self.sln.singleNumber([2, 2, 1])
         self.assertEqual(ret, 1)
