113db2769e658ece71154c64d27dad4275443e10
supercoderhawk <supercoderhawk@gmail.com>
2019-09-04 22:43:09 +0800

    python: add problem 137 and unittest

--- Python/sln_101_200/solution_131_140.py
+++ Python/sln_101_200/solution_131_140.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from typing import List
+
 
 class Solution_131_140:
     def singleNumber(self, nums):
@@ -14,3 +16,11 @@ class Solution_131_140:
             else:
                 val.pop(num)
         return list(val.keys())[0]
+
+    def singleNumber_2(self, nums: List[int]) -> int:
+        """
+        137
+        :param nums:
+        :return:
+        """
+        return (sum(set(nums)) * 3 - sum(nums)) // 2
