d29a3c4e83d4743805c409efb923ad51e145ff07
supercoderhawk <supercoderhawk@gmail.com>
2019-09-04 22:08:09 +0800

    python: add problem 102 and unittest

--- Python/sln_101_200/solution_101_110.py
+++ Python/sln_101_200/solution_101_110.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 import math
+from typing import List
+
+
 class TreeNode:
     def __init__(self, x):
         self.val = x
@@ -8,6 +11,29 @@ class TreeNode:
 
 
 class Solution_101_110(object):
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        """
+        102
+        :param root:
+        :return:
+        """
+        results = []
+
+        def recursive_tree(level, node):
+            if not node:
+                return
+            if len(results) == level:
+                level_list = [node.val]
+                results.append(level_list)
+            else:
+                results[level].append(node.val)
+
+            recursive_tree(level + 1, node.left)
+            recursive_tree(level + 1, node.right)
+
+        recursive_tree(0, root)
+        return results
+
     def maxDepth(self, root):
         """
         104
@@ -17,7 +43,7 @@ class Solution_101_110(object):
         if not root:
             return 0
 
-        left_depth = self.maxDepth(root.left)+1
-        right_depth = self.maxDepth(root.right)+1
+        left_depth = self.maxDepth(root.left) + 1
+        right_depth = self.maxDepth(root.right) + 1
 
-        return left_depth if left_depth>right_depth else right_depth
+        return left_depth if left_depth > right_depth else right_depth
