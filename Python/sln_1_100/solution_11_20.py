2213e3e523277167777676eb1818b3d81bfc936e
supercoderhawk <supercoderhawk@gmail.com>
2019-09-05 22:19:54 +0800

    python: add problem 12 and unittest

--- Python/sln_1_100/solution_11_20.py
+++ Python/sln_1_100/solution_11_20.py
@@ -10,10 +10,31 @@ class Solution_11_20(object):
         :param num:
         :return:
         """
-        num2roman = {1: 'I', 5: 'V', 10: 'X', 50: 'L', 100: 'C', 500: 'D', 1000: 'M'}
-        rets = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
-        for n in num2roman:
-            num
+        roman2num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        roman_digits = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
+        s = ''
+        for idx, roman_digit in enumerate(roman_digits[::-1], 1):
+            idx = len(roman_digits) - idx
+            if num <= 0:
+                break
+
+            digit = num // roman2num[roman_digit]
+            if digit:
+                if idx < len(roman_digits) - 1:
+                    if idx > 0 and idx % 2 and (num + roman2num[roman_digits[idx - 1]]) // roman2num[
+                        roman_digits[idx + 1]] == 1:
+                        s += roman_digits[idx - 1] + roman_digits[idx + 1]
+                        num -= roman2num[roman_digits[idx + 1]] - roman2num[roman_digits[idx - 1]]
+                    elif (idx + 1) % 2 and (num + roman2num[roman_digit]) // roman2num[roman_digits[idx + 1]] == 1:
+                        s += roman_digits[idx] + roman_digits[idx + 1]
+                        num -= roman2num[roman_digits[idx + 1]] - roman2num[roman_digit]
+                    else:
+                        s += roman_digits[idx] * digit
+                        num -= digit * roman2num[roman_digits[idx]]
+                else:
+                    s += roman_digits[idx] * digit
+                    num -= digit * roman2num[roman_digits[idx]]
+        return s
 
     def threeSumClosest(self, nums: List[int], target: int) -> int:
         """
@@ -30,7 +51,7 @@ class Solution_11_20(object):
             return sum(nums[:3])
         if sum(nums[-3:]) <= target:
             return sum(nums[-3:])
-        min_diff = sum(nums[:3])-target
+        min_diff = sum(nums[:3]) - target
         for i in range(len(nums) - 2):
             j = i + 1
             k = len(nums) - 1
--- Python/sln_1_100/test_solution_11_20.py
+++ Python/sln_1_100/test_solution_11_20.py
@@ -7,6 +7,13 @@ class Test_solution_11_20(TestCase):
     def setUp(self) -> None:
         self.sln = Solution_11_20()
 
+    def test_intToRoman(self):
+        self.assertEqual(self.sln.intToRoman(3), 'III')
+        self.assertEqual(self.sln.intToRoman(4), 'IV')
+        self.assertEqual(self.sln.intToRoman(9), 'IX')
+        self.assertEqual(self.sln.intToRoman(1944), 'MCMXLIV')
+        self.assertEqual(self.sln.intToRoman(58), 'LVIII')
+
     def test_threeSumClosest(self):
         ret = self.sln.threeSumClosest([-1, 0, 1, 1, 55], 3)
         self.assertEqual(ret, 2)
