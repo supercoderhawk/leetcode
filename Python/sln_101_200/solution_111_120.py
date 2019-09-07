34fc9e10b4de6b5f3baa1a46171778f8bb3cfd6c
supercoderhawk <supercoderhawk@gmail.com>
2019-09-07 10:00:15 +0800

    python: add problem 118 and unittest

--- Python/sln_101_200/solution_111_120.py
+++ Python/sln_101_200/solution_111_120.py
@@ -3,6 +3,28 @@ from typing import List
 
 
 class Solution_111_120(object):
+    def generate(self, numRows: int) -> List[List[int]]:
+        """
+        118
+        :param numRows:
+        :return:
+        """
+        if numRows == 0:
+            return []
+        elif numRows == 1:
+            return [[1]]
+        elif numRows == 2:
+            return [[1], [1, 1]]
+
+        results = [[1], [1, 1]]
+        for i in range(2, numRows):
+            row = [1]
+            for j in range(1, i):
+                row.append(results[i - 1][j - 1] + results[i - 1][j])
+            row.append(1)
+            results.append(row)
+        return results
+
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         """
         120
