984bf90e6da704c6157731126659cbba1daef7fa
xiayubin <xiayubin@patsnap.com>
2019-09-10 19:43:07 +0800

    python: add problem 118 and unittest

--- Python/sln_101_200/solution_111_120.py
+++ Python/sln_101_200/solution_111_120.py
@@ -45,6 +45,26 @@ class Solution_111_120(object):
             results.append(row)
         return results
 
+    def getRow(self, rowIndex: int) -> List[int]:
+        """
+        118
+        :param rowIndex:
+        :return:
+        """
+        if rowIndex == 0:
+            return [1]
+        elif rowIndex == 1:
+            return [1, 1]
+        last_row = [1, 1]
+        for row in range(2, rowIndex + 1):
+            current_row = [1]
+            for j in range(1, row):
+                current_row.append(last_row[j - 1] + last_row[j])
+            current_row.append(1)
+            last_row = current_row
+        return last_row
+
+
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         """
         120
