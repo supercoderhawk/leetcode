bd71716344d52083cba1ea80569a4f7b68d61885
supercoderhawk <supercoderhawk@gmail.com>
2019-11-05 22:42:13 +0800

    python: add problem 31 and unittest

--- Python/sln_1_100/solution_31_40.py
+++ Python/sln_1_100/solution_31_40.py
@@ -1,6 +1,37 @@
 # -*- coding: UTF-8 -*-
+from typing import List
+
 
 class Solution_31_40:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        31
+        Do not return anything, modify nums in-place instead.
+        """
+        if len(nums) <= 1:
+            return
+        num_len = len(nums)
+        if nums[num_len - 2] < nums[num_len - 1]:
+            nums[num_len - 1], nums[num_len - 2] = nums[num_len - 2], nums[num_len - 1]
+        else:
+            start_idx = -1
+            for idx in range(num_len - 1, 0, -1):
+                if nums[idx - 1] < nums[idx]:
+                    start_idx = idx - 1
+                    break
+            if start_idx == -1:
+                for idx in range(0, num_len // 2):
+                    nums[idx], nums[num_len - 1 - idx] = nums[num_len - 1 - idx], nums[idx]
+            else:
+                end_idx = num_len - 1
+                for idx in range(start_idx + 1, num_len):
+                    if nums[start_idx] >= nums[idx]:
+                        end_idx = idx - 1
+                        break
+                nums[start_idx], nums[end_idx] = nums[end_idx], nums[start_idx]
+                for idx in range(start_idx + 1, start_idx + 1 + (num_len - 1 - start_idx) // 2):
+                    nums[idx], nums[num_len + start_idx - idx] = nums[num_len + start_idx - idx], nums[idx]
+
     def searchInsert(self, nums, target):
         """
         35
@@ -41,3 +72,11 @@ class Solution_31_40:
                     count = 1
             curr += str(count) + say
         return curr
+
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        """
+        39
+        :param candidates:
+        :param target:
+        :return:
+        """
--- Python/sln_1_100/test_solution_31_40.py
+++ Python/sln_1_100/test_solution_31_40.py
@@ -8,6 +8,23 @@ class TestSolution_31_40(TestCase):
     def setUp(self):
         self.sln = Solution_31_40()
 
+    def test_nextPermutation(self):
+        nums1 = [1, 1, 5]
+        self.sln.nextPermutation(nums1)
+        assert nums1 == [1, 5, 1]
+        nums2 = [3, 2, 1]
+        self.sln.nextPermutation(nums2)
+        assert nums2 == [1, 2, 3]
+        nums3 = [1, 3, 2]
+        self.sln.nextPermutation(nums3)
+        assert nums3 == [2, 1, 3]
+        num4 = [1, 5, 1]
+        self.sln.nextPermutation(num4)
+        assert num4 == [5, 1, 1]
+        nums5 = [5, 4, 7, 5, 3, 2]
+        self.sln.nextPermutation(nums5)
+        assert nums5 == [5, 5, 2, 3, 4, 7]
+
     def test_searchInsert(self):
         ret1 = self.sln.searchInsert([1, 3, 5, 6], 5)
         self.assertEqual(ret1, 2)
